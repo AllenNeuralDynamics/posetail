893b7674cba8404b57d3e3f37bad4828652af109
Lili Karashchuk <krchtchk@gmail.com>
2025-12-05 13:49:37 -0500

    commit posetail dataset

--- posetail/datasets/posetail_dataset.py
+++ posetail/datasets/posetail_dataset.py
@@ -94,7 +94,7 @@ class PosetailDataset(Dataset):
         views = []
 
         if len(cam_names) > 5:
-            ix_cams = np.random.choice(len(cam_names), size=5)
+            ix_cams = np.random.choice(len(cam_names), size=5, replace=False)
             # ix_cams = np.arange(6)
             cam_names = [cam_names[i] for i in ix_cams]
 
@@ -116,8 +116,8 @@ class PosetailDataset(Dataset):
 
         coords = coords[:, :, good[0]]
             
-        if coords.shape[2] > 128:
-            ix_p = np.random.choice(coords.shape[2], size=128)
+        if coords.shape[2] > 60:
+            ix_p = np.random.choice(coords.shape[2], size=60, replace=False)
             # ix_p = np.arange(60)
             coords = coords[:, :, ix_p, :]
         
@@ -146,7 +146,7 @@ class PosetailDataset(Dataset):
         # print(views[0].shape)
         # views = [torch.stack(v, axis = 0) for v in views]
 
-            
+        # print(views, coords, fnums, cgroup)
         return views, coords, fnums, cgroup
 
 
