a906cc6efef2f3055af316e9d80ef30f10edc1d6
k4ru2 <165200871+k4ru2@users.noreply.github.com>
2026-01-27 22:57:52 +0000

    bsub submission file

---
+++ train.sh
@@ -0,0 +1,39 @@
+#BSUB -J posetail 					# job name
+#BSUB -W 00:10 						# walltime in HH:MM or MMMM
+#BSUB -n 8 							# num CPU slots
+#BSUB -M 64000 						# memory limit in MB
+#BSUB -e %J.err 					# error file
+#BSUB -o %J.out 					# out file
+#BSUB -R "span[hosts=1]" 			# number of nodes spanned by gpus
+#BSUB -q gpu_h100 					# type of gpu
+#BSUB -gpu "num=8" 					# number of gpus
+
+# define model params
+CONFIG_PATH=${1:-"configs/config_default_3d_offsets.toml"}
+NUM_GPUS=${2:-4}
+PRECISION=${3:-"32"}
+STRATEGY=${4:-"ddp"}
+echo "using config $CONFIG_PATH"
+echo "pwd: $PWD"
+
+# gpu specs
+nvidia-smi
+
+# pixi environment 
+export PATH="$HOME/.pixi/bin:$PATH"
+pixi --version
+pixi install 
+echo "pixi env setup complete"
+
+# wandb login 
+wandb login $WANDB
+
+# run training script 
+echo "starting training..."
+
+echo "done!"
+pixi run python train.py \
+	--config-path "${CONFIG_PATH}" \
+ 	--num_nodes ${NUM_GPUS} \
+ 	--precision ${PRECISION} \
+ 	--strategy ${STRATEGY}
