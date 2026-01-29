ac6bdc6f0df2d13237d1bfe879da521116d7468e
Lili Karashchuk <krchtchk@gmail.com>
2026-01-29 11:39:33 -0500

    various changes to train.sh

--- train.sh
+++ train.sh
@@ -1,11 +1,12 @@
-#BSUB -J posetail 					# job name
+#!/bin/bash
+#BSUB -J tribig 					# job name
 #BSUB -W 00:10 						# walltime in HH:MM or MMMM
-#BSUB -n 8 							# num CPU slots
+#BSUB -n 96 							# num CPU slots
 #BSUB -M 64000 						# memory limit in MB
-#BSUB -e ../../results/lsf/%J.err 	# error file
-#BSUB -o ../../results/lsf/%J.out 	# out file
+#BSUB -e /groups/karashchuk/home/karashchukl/logs/posetail/%J.err 	# error file
+#BSUB -o /groups/karashchuk/home/karashchukl/logs/posetail/%J.out 	# out file
 #BSUB -R "span[hosts=1]" 			# number of nodes spanned by gpus
-#BSUB -q gpu_h100 					# type of gpu
+#BSUB -q gpu_h200_parallel   		# type of gpu
 #BSUB -gpu "num=8" 					# number of gpus
 
 # define model params
@@ -16,19 +17,23 @@ STRATEGY=${4:-"ddp"}
 echo "using config $CONFIG_PATH"
 echo "pwd: $PWD"
 
+cd /groups/karashchuk/home/karashchukl/projects/posetail
+
+source setup_env_vars.sh
+
 # gpu specs
 nvidia-smi
 
 # pixi environment 
 export PATH="$HOME/.pixi/bin:$PATH"
 pixi --version
-pixi install 
-echo "pixi env setup complete"
+# pixi install 
+# echo "pixi env setup complete"
 
 # wandb login 
-wandb login $WANDB
+# wandb login $WANDB
 
 # run training script 
 echo "starting training..."
-pixi run python train.py --config-path "${CONFIG_PATH}" --num_nodes ${NUM_GPUS} --precision ${PRECISION} --strategy ${STRATEGY}
+pixi run python train.py --config-path "${CONFIG_PATH}" --num_nodes ${NUM_GPUS} --precision ${PRECISION} --strategy ${STRATEGY} --devices 0 1 2 3 4 5 6 7
 echo "done!"
