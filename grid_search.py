dd2aec307e49aab66c248fc39cc83b1b6a6aa6e4
k4ru2 <165200871+k4ru2@users.noreply.github.com>
2026-02-02 19:47:12 +0000

    update grid search with bsub submission

--- grid_search.py
+++ grid_search.py
@@ -38,8 +38,8 @@ def parse_args():
     ''' 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--config-path', default = './configs/config_default.toml')
-    parser.add_argument('--auto-submit', action = 'store_true', help = 'auto submits job using train.sh')
+    parser.add_argument('--config-path', default = './configs/config_defaul_3d.toml')
+    parser.add_argument('--auto-submit', action = 'store_true', help = 'auto submits job to lsf')
 
     args = parser.parse_args()
 
@@ -160,4 +160,15 @@ if __name__ == '__main__':
         for config_path in config_paths:
 
             print(f'submitting job with config {config_path}')
-            result = subprocess.run(f'bsub {config_path} < train.sh', shell = True, check = True)
+
+            subprocess.run([
+                'bsub', 
+                '-J' 'triexp', 
+                '-e', '/groups/karashchuk/home/ruppk2/results/lsf/%J.err', 
+                '-o', '/groups/karashchuk/home/ruppk2/results/lsf/%J.out', 
+                '-n', '96', 
+                '-q', 'gpu_a100', 
+                '-gpu', 'num=8', 
+                'pixi', 'run', 'python', 'train.py', 
+                '--config-path', config_path], check = True
+            )
