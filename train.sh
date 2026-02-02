ecb523877289cde08b71b1b5bc45842d0933e342
k4ru2 <165200871+k4ru2@users.noreply.github.com>
2026-02-02 20:40:03 +0000

    env vars file

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #BSUB -J tribig 					# job name
 #BSUB -W 00:10 						# walltime in HH:MM or MMMM
-#BSUB -n 96 						# num CPU slots
+#BSUB -n 96 							# num CPU slots
 #BSUB -M 64000 						# memory limit in MB
 #BSUB -e ~/logs/posetail/%J.err 	# error file
 #BSUB -o ~/logs/posetail/%J.out 	# out file
@@ -14,7 +14,6 @@ CONFIG_PATH=${1:-"configs/config_default_3d.toml"}
 NUM_NODES=${2:-1}
 PRECISION=${3:-"32"}
 STRATEGY=${4:-"ddp"}
-
 echo "using config $CONFIG_PATH"
 echo "pwd: $PWD"
 
@@ -27,7 +26,8 @@ pixi --version
 pixi install 
 echo "pixi env setup complete"
 
-wandb login 
+# wandb login 
+source ~/.env_vars
 wandb login $WANDB
 
 # run training script 
