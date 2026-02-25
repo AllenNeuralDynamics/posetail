da78b0d2a49cd1140ceaa089893e9f2eb4d1bf0a
k4ru2 <165200871+k4ru2@users.noreply.github.com>
2026-02-25 21:34:09 +0000

    commit before merging

--- posetail/datasets/posetail_dataset.py
+++ posetail/datasets/posetail_dataset.py
@@ -402,7 +402,11 @@ class PosetailDataset(Dataset):
         rows = []
         mode = '3d' # if track_3d else '2d' - not yet implemented
 
-        for dataset in get_dirs(self.data_path): 
+        for dataset in get_dirs(self.data_path):
+
+            #TODO: remove later 
+            if dataset == 'kubric-multiview': 
+                continue
             
             # NOTE: split folder structure must match here
             dataset_path = os.path.join(self.data_path, dataset, self.split_dir)
